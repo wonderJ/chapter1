9d78aabed10074966c6ffdc4675042453ae4d73f
1565880895@qq.com <1565880895@qq.com>
2016-11-22 18:27:09 +0800

    chapter

---
+++ src/main/java/org/smart4j/chapter1/model/Customer.java
@@ -0,0 +1,70 @@
+package org.smart4j.chapter1.model;
+
+/**
+ * Created by 陈亮 on 2016/11/22.
+ */
+public class Customer {
+    private int id;
+    private String name;
+    private String contact;
+    private String telphone;
+    private String email;
+    private String remark;
+    private String del_flag;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    public String getTelphone() {
+        return telphone;
+    }
+
+    public void setTelphone(String telphone) {
+        this.telphone = telphone;
+    }
+
+    public String getDel_flag() {
+        return del_flag;
+    }
+
+    public void setDel_flag(String del_flag) {
+        this.del_flag = del_flag;
+    }
+}
